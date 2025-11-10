97ab1608505687240eb8c27ad78ae2c4e61d80a4
Lukas Tomasek <vedstav31@gmail.com>
2025-11-09 22:13:08 -0500

    setup project

--- vault-ai/ViewController.swift
+++ vault-ai/ViewController.swift
@@ -5,15 +5,24 @@
 //  Created by Lukas Tomasek on 2025-11-09.
 //
 
+import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
+    private let label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .white
+
+        label.text = "Vault AI"
+        label.textColor = .black
+
+        view.addSubview(label)
+
+        label.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
-
-
 }
-
