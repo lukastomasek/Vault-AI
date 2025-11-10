dc34a280c1dfccd05d4b6fe21f0e984e057a1bf7
Lukas Tomasek <vedstav31@gmail.com>
2025-11-09 22:04:54 -0500

    Initial Commit

---
+++ vault-ai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vault-ai
+//
+//  Created by Lukas Tomasek on 2025-11-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
